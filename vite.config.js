25d052dbb158ab4d2194bf582e4c43caef33de01
giaongo <83873333+giaongo@users.noreply.github.com>
2026-04-29 16:50:25 +0300

    Change the port number

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
         tailwindcss(),
     ],
     server: {
+        allowedHosts: 'all',
         watch: {
             ignored: ['**/storage/framework/views/**'],
         },
